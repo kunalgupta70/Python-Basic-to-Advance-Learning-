336439e3c2cb55af450d52363a87ca5ba497f043
KUNAL GUPTA <149429988+kunalgupta70@users.noreply.github.com>
2024-10-21 18:21:18 +0000

    This is conditional statement code

---
+++ ifElse.py
@@ -0,0 +1,14 @@
+#if statement
+#if statement works only for true condition
+a = 26
+b = 108
+if b > a:
+   print("b is greater than a")
+
+age = 26
+if age > 19:
+    print("Your are an adult")
+
+age = int(input("Enter your age: "))
+if age > 19:
+   print("Your are an adult")    
